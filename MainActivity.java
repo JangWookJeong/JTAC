7c4fd179138b99d065756b934bdeff3f1e767b64
Jack <codename444@naver.com>
2018-09-10 23:24:31 +0900

    프로젝트 시작 18.09.10

--- MainActivity.java
+++ MainActivity.java
@@ -2,15 +2,14 @@ package project.codename.jtac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
+        Project Main Screen
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }/////onCreate
-
-
 }/////MainActivity
