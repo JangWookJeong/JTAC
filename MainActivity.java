22bb15322d85324cb0b0719e2c6d6c46e8201701
Jack <codename444@naver.com>
2018-09-10 23:27:52 +0900

    프로젝트 시작 18.09.10

--- MainActivity.java
+++ MainActivity.java
@@ -7,9 +7,36 @@ public class MainActivity extends AppCompatActivity {
     /*
         Project Main Screen
     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }/////onCreate
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }/////onStart
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }/////onStop
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }/////onDestroy
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }/////onPause
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }/////onResume
+
 }/////MainActivity
