8271efc3d70c5a402b0ca1376ccdf8710639f85e
Jack <codename444@naver.com>
2018-09-10 21:53:09 +0900

    욱정이짱 추가

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package project.codename.jtac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        string();
+    }/////onCreate
+
+    private void string() {
+
+        Toast.makeText(this, "욱정이짱 ㅎ", Toast.LENGTH_SHORT).show();
+    }
+
+}/////MainActivity
