3ab38ba7b1191764afef3c8a87af5301b8223919
Jack <codename444@naver.com>
2018-09-10 22:01:19 +0900

    테스트메서드 지우고 프로젝트 준비.

--- MainActivity.java
+++ MainActivity.java
@@ -10,26 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        string();
-        
-        s();
-        
-        delete();
     }/////onCreate
 
-    private void delete() {
-        Toast.makeText(this, "인생을 지우다ㅓ.씨발럼들아", Toast.LENGTH_SHORT).show();
-    }
-
-    private void s() {
-        Toast.makeText(this, "호호", Toast.LENGTH_SHORT).show();
-    }
-
-    private void string() {
-
-        Toast.makeText(this, "욱정이짱 ㅎ", Toast.LENGTH_SHORT).show();
-
-    }
 
 }/////MainActivity
